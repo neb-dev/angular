d925effea27ea9fdde001ee6b52e5114e3956ab7
neb <jneb.dev@outlook.com>
2025-05-16 17:41:20 -0600

    removes default code, adds header component, adds png

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true, // this prop is true by default
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {}
