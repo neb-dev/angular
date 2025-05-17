9b190c2b88d98aa8e0de3749f0ba645026175e7e
neb <jneb.dev@outlook.com>
2025-05-17 14:36:12 -0600

    imports user component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular';
+  users = DUMMY_USERS;
+
+  onSelect(id: string) {
+    console.log(id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  output,
+} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return '/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
+
+// avatar = input.required<string>();
+// name = input.required<string>();
+// id = input.required<string>();
+
+// select = output<string>();
+
+// imagePath = computed(() => {
+//   return 'users/' + this.avatar();
+// });
