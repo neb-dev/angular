4bdbf74c9c794cc76b045158387c65526bab519c
neb <jneb.dev@outlook.com>
2025-05-22 12:06:56 -0600

    adds legacy for and else if comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS, DummyUser } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+// import { NgFor, NgIf } from '@angular/common'; legacy if-else and for in app html
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent], // NgIf, NgFor
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
