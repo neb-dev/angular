80e09ebfa5019288b1cce33d1718010a15c5005b
neb <jneb.dev@outlook.com>
2025-05-24 17:13:11 -0600

    adds new-task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { DummyUser } from '../dummy-users';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
@@ -13,7 +14,11 @@ import { DUMMY_TASKS } from '../dummy-tasks';
 export class TasksComponent {
   user = input.required<DummyUser>(); // (typeof DUMMY_USERS)[number]
   // @Input() user: DummyUser | null = null;
-  tasks = DUMMY_TASKS;
+  tasks = DUMMY_TASKS; // reference
+  // tasks = [...DUMMY_TASKS] // shallow copy, references nested obj and arr props
+  // tasks = structuredClone(DUMMY_TASKS) // deep (modern) copy - everything is copied
+
+  showNewTaskForm = false;
 
   get userName() {
     return this.user().name;
@@ -24,8 +29,11 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.user().id);
   }
 
+  get openNewTaskForm() {
+    return (this.showNewTaskForm = true);
+  }
+
   completeTask(taskId: string) {
-    console.log('task id: ' + taskId);
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
   }
 }
