7bba31a9dc48b091de53649108a670c2c1eff35d
neb <jneb.dev@outlook.com>
2025-05-25 14:38:56 -0600

    adds handling for adding new tasks

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTask } from '../tasks.types';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  newTask: NewTask = {
+    title: '',
+    summary: '',
+    dueDate: '',
+  };
+  closeDialog = output();
+  submittedTask = output<NewTask>(); // typeof this.newTask
 
+  handleCloseDialog() {
+    this.closeDialog.emit();
+  }
+
+  handleSubmit() {
+    this.submittedTask.emit(this.newTask);
+  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { DummyUser } from '../dummy-users';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from '../dummy-tasks';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTask } from './tasks.types';
 
 @Component({
   selector: 'app-tasks',
@@ -17,8 +18,7 @@ export class TasksComponent {
   tasks = DUMMY_TASKS; // reference
   // tasks = [...DUMMY_TASKS] // shallow copy, references nested obj and arr props
   // tasks = structuredClone(DUMMY_TASKS) // deep (modern) copy - everything is copied
-
-  showNewTaskForm = false;
+  showNewTaskDialog = false;
 
   get userName() {
     return this.user().name;
@@ -29,11 +29,25 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.user().id);
   }
 
-  get openNewTaskForm() {
-    return (this.showNewTaskForm = true);
+  get openNewTaskDialog() {
+    return (this.showNewTaskDialog = true);
+  }
+
+  get closeNewTaskDialog() {
+    return (this.showNewTaskDialog = false);
   }
 
   completeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  addNewTask(task: NewTask) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.user().id,
+      title: task.title,
+      summary: task.summary,
+      dueDate: task.dueDate,
+    });
   }
 }
